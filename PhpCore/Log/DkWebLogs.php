d74c0ed253dd7bc72a535dc8324222e4952c6d85
darkcompet <darkcompet@gmail.com>
2020-04-10 10:21:41 +0700

    add dump

--- PhpCore/Log/DkWebLogs.php
+++ PhpCore/Log/DkWebLogs.php
@@ -3,4 +3,12 @@
 namespace App\Tool\PhpCore\Log;
 
 class DkWebLogs extends AbsLogs {
+   /**
+    * Dump and stop page to see result.
+    * @param $obj
+    */
+   public static function dump($obj) {
+      var_dump($obj);
+      die();
+   }
 }
