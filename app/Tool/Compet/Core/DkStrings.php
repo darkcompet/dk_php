34815ddda5fc16a09615b8f12dfa7fc55afd0b58
Compet <darkcompet@gmail.com>
2021-05-11 11:41:07 +0700

    Add starts, ends with for string

--- app/Tool/Compet/Core/DkStrings.php
+++ app/Tool/Compet/Core/DkStrings.php
@@ -8,17 +8,24 @@ namespace App\Tool\Compet\Core;
  * @author darkcompet
  */
 class DkStrings {
+	/**
+	 * @return int Length of string (char count).
+	 */
+	public static function length($text) {
+		return mb_strlen($text);
+	}
+	
     /**
      * @param string $text
-     * @param string $targetChar character should be trimed
-     * @return false|string
+     * @param string $targetChar Delimiter char.
+     * @return string|false
      */
     public static function trim($text, $targetChar) {
         if ($text == null) {
             return null;
         }
 
-        $N = strlen($text);
+        $N = mb_strlen($text);
         $start = 0;
         $end = $N - 1;
 
@@ -29,17 +36,16 @@ class DkStrings {
             --$end;
         }
 
-        return ($start > 0 || $end < $N - 1) ? substr($text, $start, $end - $start + 1) : $text;
+        return ($start > 0 || $end < $N - 1) ? self::substring($text, $start, $end - $start + 1) : $text;
     }
 
     /**
      * @param string $text
      * @param string|null $delimiter
-     * @return array|string return false if `delimiter` is null or empty
-     *                otherwise return array of string.
+     * @return array|false False if `delimiter` is null or empty. Otherwise return array of string.
      */
     public static function split($text, $delimiter) {
-        if (!$text || !$delimiter) {
+        if (! $text || ! $delimiter) {
             return $text;
         }
         return explode($delimiter, $text);
@@ -55,24 +61,50 @@ class DkStrings {
     }
 
     /**
-     * @param string $text target string
-     * @param int $startIndex inclusive index
-     * @param int $endIndex exclusive index
-     * @return false|string
+	 * Get substring from an index to a position.
+	 * 
+     * @param string $text Target string
+     * @param int $startIndex Inclusive index, should <= `endIndex` and should NOT be negative.
+     * @param int $endIndex Exclusive index, should >= `startIndex`.
+     * @return string
      */
     public static function substring($text, $startIndex, $endIndex) {
-        return substr($text, $startIndex, $endIndex - $startIndex + 1);
+        return substr($text, $startIndex, $endIndex - $startIndex);
     }
 
     /**
      * @param string $text
      * @param string $target
      * @param string $replacement
-     * @return string|string[]
+     * @return string|string[] Result string, or array for multiple replacement.
      */
     public static function replace($text, $target, $replacement) {
         return str_replace($target, $replacement, $text);
     }
+	
+	/**
+	 * @return boolean True iff the `text` is started with given `target`.
+	 */
+	public static function startsWith($text, $sample) {
+		$sampleCount = mb_strlen($sample);
+		$totalCount = mb_strlen($text);
+		if ($sampleCount > $totalCount) {
+			return false;
+		}
+		return $sample === self::substring($text, 0, $sampleCount);
+	}
+	
+	/**
+	 * @return boolean True iff the `text` is started with given `sample`.
+	 */
+	public static function endsWith($text, $sample) {
+		$totalCount = mb_strlen($text);
+		$fromIndex = $totalCount - mb_strlen($sample);
+		if ($fromIndex < 0) {
+			return false;
+		}
+		return $sample === self::substring($text, $fromIndex, $totalCount);
+	}
 
     /**
      * Replace a substring which be expressed in regular expression with a replacement in given text.
@@ -87,11 +119,11 @@ class DkStrings {
     }
 
     /**
-     * @param string $format like "print %d fields in class %s"
-     * @param array $args list of argument
+     * @param string $format For eg,. "There are %d fields in class %s"
+     * @param array $args List of argument
      * @return string
      */
-    public static function format($format, $args) {
+    public static function format($format, ...$args) {
         return sprintf($format, $args);
     }
 
